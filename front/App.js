54fe31b8664c24ad3c5318a6f5a3675ff89c84d5
조현우 <chw03177@gmail.com>
2024-03-11 10:25:39 +0900

    [FE] Fix : 뭔가 이상했는데 고침

--- front/App.js
+++ front/App.js
@@ -1,10 +1,18 @@
-import { GluestackUIProvider, Text } from "@gluestack-ui/themed"
-import { config } from "@gluestack-ui/config" // Optional if you want to use default theme
+import {
+  GluestackUIProvider,
+  Text,
+  Button,
+  ButtonText,
+} from '@gluestack-ui/themed';
+import {config} from '@gluestack-ui/config';
 
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
+      <Button>
+        <ButtonText>Hello world</ButtonText>
+      </Button>
       <Text>Hello World!</Text>
     </GluestackUIProvider>
-  )
+  );
 }
