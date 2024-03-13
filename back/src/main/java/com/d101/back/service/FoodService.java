e4ecfe36f83d2c64b656b2a96645d79a76c147b2
sarang682 <jms20914@gmail.com>
2024-03-13 14:10:28 +0900

    [BE] Refactor : food service

--- back/src/main/java/com/d101/back/service/FoodService.java
+++ back/src/main/java/com/d101/back/service/FoodService.java
@@ -4,41 +4,32 @@ import com.d101.back.dto.FoodDto;
 import com.d101.back.entity.Food;
 import com.d101.back.repository.FoodRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class FoodService {
     private final FoodRepository foodRepository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public List<FoodDto> searchByName(String name) {
         return foodRepository.findByNameContainingOrderByNameAsc(name, PageRequest.of(0, 30)).stream()
-                .map(temp -> {
-                    FoodDto input = new FoodDto();
-                    BeanUtils.copyProperties(temp, input);
-                    return input;
-                })
-                .collect(Collectors.toList());
+                .map(FoodDto::fromEntity)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<FoodDto> rankingByEmail(String email) {
+        return foodRepository.rankingByEmail(email, PageRequest.of(0, 10)).stream()
+                .map(FoodDto::fromEntity)
+                .toList();
     }
 
     @Transactional
-    public List<FoodDto> rankingByEmail(String email) {
-        return foodRepository.rankingByEmail(email, PageRequest.of(0, 10)).stream()
-                .map(temp -> {
-                    FoodDto input = new FoodDto();
-                    BeanUtils.copyProperties(temp, input);
-                    return input;
-                })
-                .collect(Collectors.toList());
-    }
-
     public void addFood(FoodDto foodDto) {
         Food food = Food.builder()
                 .calorie(foodDto.getCalorie())
@@ -52,7 +43,6 @@ public class FoodService {
                 .saturatedFat(foodDto.getSaturatedFat())
                 .transFat(foodDto.getTransFat())
                 .build();
-
         foodRepository.save(food);
     }
 }
