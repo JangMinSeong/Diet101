738bc188e0cccab53cf09060e636219623a54841
Dongyoung <doyokim75@gmail.com>
2024-03-14 14:05:03 +0900

    [BE] Refact : DietController URL

--- back/src/main/java/com/d101/back/controller/DietController.java
+++ back/src/main/java/com/d101/back/controller/DietController.java
@@ -23,19 +23,19 @@ import org.springframework.web.multipart.MultipartFile;
 public class DietController {
 	private final DietService dietService;
 	
-	@GetMapping("/info")
+	@GetMapping("")
 	public ResponseEntity<?> getTodayDietInfo(Authentication authentication) {
 		List<MealDto> meals = dietService.getMealsForSpecificDate(authentication.getName(), LocalDate.now().toString());
 		return new ResponseEntity<>(meals, HttpStatus.OK);
 	}
 	
-	@GetMapping("/info/date")
+	@GetMapping("/date")
 	public ResponseEntity<?> getDietInfoForDate(@RequestParam(value = "date") String date, Authentication authentication) {
 		List<MealDto> meals = dietService.getMealsForSpecificDate(authentication.getName(), date);
 		return new ResponseEntity<>(meals, HttpStatus.OK);
 	}
 	
-	@GetMapping("/info/term")
+	@GetMapping("/term")
 	public ResponseEntity<?> getDietInfoForTerm(@RequestParam(value = "dateFrom") String dateFrom, @RequestParam(value = "dateTo") String dateTo, Authentication authentication) {
 		List<MealDto> meals = dietService.getMealsForSpecificTerm(authentication.getName(), dateFrom, dateTo);
 		return new ResponseEntity<>(meals, HttpStatus.OK);
@@ -47,13 +47,13 @@ public class DietController {
 		return ResponseEntity.ok("Success");
 	}
 
-	@GetMapping("/info/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<?> getDietDetail(@PathVariable(value = "id") Long id, Authentication authentication) {
 		MealDto meal = dietService.getMealOfUserById(authentication.getName(), id);
 		return new ResponseEntity<>(meal, HttpStatus.OK);
 	}
 
-	@GetMapping("/info/{meal_id}/{food_id}")
+	@GetMapping("/{meal_id}/{food_id}")
 	public ResponseEntity<?> getFoodFromDiet(@PathVariable(value = "meal_id") Long meal_id, @PathVariable(value = "food_id") Long food_id, Authentication authentication) {
 		IntakeDto intake = dietService.getFoodDto(meal_id, food_id);
 		return new ResponseEntity<>(intake, HttpStatus.OK);
