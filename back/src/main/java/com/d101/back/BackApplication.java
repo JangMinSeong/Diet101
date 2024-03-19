c97c93a7cc1c8420bcaf6627ffc488359d3730e2
bogeun <kimbo3212@gmail.com>
2024-03-19 15:48:59 +0900

    [Infra] check the jwt key

--- back/src/main/java/com/d101/back/BackApplication.java
+++ back/src/main/java/com/d101/back/BackApplication.java
@@ -1,5 +1,7 @@
 package com.d101.back;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,9 +9,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class BackApplication {
+    private static final Logger log = LoggerFactory.getLogger(BackApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(BackApplication.class, args);
+        log.info("JWT_SECRET: {}", System.getenv("JWT_SECRET"));
     }
 
 }
