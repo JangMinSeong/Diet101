57e54be1abbed1524d650d9bd94476b864bf4f1b
sarang682 <jms20914@gmail.com>
2024-03-26 14:30:42 +0900

    [BE] Feat : 음식 추천 기능

---
+++ ai/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine, MetaData, Table, select
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+engine = create_engine(os.environ["DATABASE_URL"])
+Session = sessionmaker(bind=engine)
+session = Session()
+metadata = MetaData()
+metadata.bind = engine
+
+food_table = Table('food', metadata, autoload_with=engine)
+preference_table = Table('preference', metadata, autoload_with=engine)
+
+def getFoodListByKcal(kcal: int):
+    query = select(food_table.c.food_id).where(food_table.c.calorie <= kcal)
+    return session.execute(query).fetchall()
+
+def getPreferenceList():
+    query = select(preference_table)
+    return session.execute(query).fetchall()
