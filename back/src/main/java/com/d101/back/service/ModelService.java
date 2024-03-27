33810a3488165ad50a61dba5167e1d52be2bf2e9
Dongyoung <doyokim75@gmail.com>
2024-03-27 13:14:35 +0900

    [BE] fix : build.gradle apache POI

--- back/src/main/java/com/d101/back/service/ModelService.java
+++ back/src/main/java/com/d101/back/service/ModelService.java
@@ -2,6 +2,7 @@ package com.d101.back.service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.*;
@@ -18,6 +19,9 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class ModelService {
 
+    @Value("${ai.server.url}")
+    private String url;
+
     public String transmitImageToYolo(MultipartFile multipartFile) throws IOException {
         RestTemplate restTemplate = new RestTemplate();
 
@@ -32,14 +36,12 @@ public class ModelService {
             }
         };
 
-        body.add("file", resource); // 'image'는 서버에서 파일을 받는 파라미터명과 일치해야 합니다.
+        body.add("file", resource); // 'image'는 서버에서 파일을 받는 파라미터명과 일치해야 함
 
         HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
-        // 외부 서버의 엔드포인트로 POST 요청을 보냅니다.
-        ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8000/model/checkcal", requestEntity, String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity(url + "/model/checkcal", requestEntity, String.class);
 
-        // 응답으로 받은 JSON 문자열을 반환합니다.
         return response.getBody();
     }
 
@@ -57,15 +59,13 @@ public class ModelService {
             }
         };
 
-        body.add("file", resource); // 'image'는 서버에서 파일을 받는 파라미터명과 일치해야 합니다.
+        body.add("file", resource); // 'image'는 서버에서 파일을 받는 파라미터명과 일치해야 함
 
         HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
-        // 외부 서버의 엔드포인트로 POST 요청을 보냅니다.
         ResponseEntity<String> response =
-                restTemplate.postForEntity("http://localhost:8000/model/ocr", requestEntity, String.class);
+                restTemplate.postForEntity(url + "/model/ocr", requestEntity, String.class);
 
-        // 응답으로 받은 JSON 문자열을 반환합니다.
         return response.getBody();
     }
 }
