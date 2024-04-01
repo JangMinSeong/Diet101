36414673e22e94ffc65e0ffcfd1bc6c2db52e279
Dongyoung <doyokim75@gmail.com>
2024-04-01 17:49:22 +0900

    [FE] Feat : ModelRepository.kt, ModelService.kt 생성

---
+++ front/D101/app/src/main/java/com/ssafy/d101/model/YoloResponse.kt
@@ -0,0 +1,17 @@
+package com.ssafy.d101.model
+
+data class YoloResponse(
+    val tag: String,
+    val left_top: List<Double>,
+    val width: Double,
+    val height: Double,
+    val yoloFoodDto: YoloFood
+)
+
+
+data class YoloFood(
+    val calorie: Int,
+    val carbohydrate: Double,
+    val protein: Double,
+    val fat: Double
+)
