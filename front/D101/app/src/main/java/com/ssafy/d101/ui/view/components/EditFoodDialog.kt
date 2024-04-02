a610973f8a49fc70241ca0b59b968a4dfd2a2581
장민성 <hs0765@naver.com>
2024-04-02 17:14:19 +0900

    [FE] Feat : change EditFoodDialog

--- front/D101/app/src/main/java/com/ssafy/d101/ui/view/components/EditFoodDialog.kt
+++ front/D101/app/src/main/java/com/ssafy/d101/ui/view/components/EditFoodDialog.kt
@@ -1,7 +1,10 @@
 package com.ssafy.d101.ui.view.components
 
 import android.util.Log
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,13 +13,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -30,9 +36,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ssafy.d101.model.FoodInfo
 import com.ssafy.d101.viewmodel.FoodSearchViewModel
@@ -66,60 +77,118 @@ fun EditFoodDialog(showDialog: Boolean, onDismiss: () -> Unit, modelViewModel: M
             title = { Text("음식 수정하기") },
             text = {
                 Column {
-                    TextField(
-                        value = searchQuery,
-                        onValueChange = { searchQuery = it },
-                        label = { Text("음식 검색") },
-                        trailingIcon = {
-                            IconButton(onClick = {
-                                foodSearchViewModel.fetchFoodItems(searchQuery)
-                                if(foodLists.isEmpty())
-                                    showDropdown = false
-                                else
-                                    showDropdown = true
-                            }) {
-                                Icon(Icons.Default.Search, contentDescription = "검색")
+
+                    Box() {
+                        TextField(
+                            modifier = Modifier.width(300.dp),
+                            value = searchQuery,
+                            onValueChange = { searchQuery = it },
+                            label = { Text("음식 검색") },
+                            trailingIcon = {
+                                IconButton(onClick = {
+                                    foodSearchViewModel.fetchFoodItems(searchQuery)
+                                    if (foodLists.isEmpty())
+                                        showDropdown = false
+                                    else
+                                        showDropdown = true
+                                }) {
+                                    Icon(Icons.Default.Search, contentDescription = "검색")
+                                }
                             }
-                        }
-                    )
-                    if (showDropdown && foodLists.isNotEmpty()) {
-                        // 검색 결과가 있고, 드롭다운을 보여줘야 할 경우
-                        ShowDropdownMenu(foodLists, onItemSelected = { food ->
-                            // 선택한 음식으로 필드 업데이트
-                                            // 아이템 클릭 시 필드 업데이트
-                                            searchQuery = food.name // 실제 음식 이름으로 검색 쿼리 업데이트
-                                            foodName = food.name
-                                            carbs = food.carbohydrate.toString()
-                                            protein = food.protein.toString()
-                                            fat = food.fat.toString()
-                                            calorie = food.calorie.toString()
-                                            showDropdown = false // 검색 결과 숨김
-                                        }
-                                    )
+                        )
+                        if (showDropdown && foodLists.isNotEmpty()) {
+                            // 검색 결과가 있고, 드롭다운을 보여줘야 할 경우
 
+                            ShowDropdownMenu(foodLists, onItemSelected = { food ->
+                                // 선택한 음식으로 필드 업데이트
+                                // 아이템 클릭 시 필드 업데이트
+                                searchQuery = food.name // 실제 음식 이름으로 검색 쿼리 업데이트
+                                foodName = food.name
+                                carbs = food.carbohydrate.toString()
+                                protein = food.protein.toString()
+                                fat = food.fat.toString()
+                                calorie = food.calorie.toString()
+                                showDropdown = false // 검색 결과 숨김
+                            }
+                            )
+
+                        }
                     }
-                    Spacer(modifier = Modifier.height(10.dp))
-
+                    Spacer(modifier = Modifier.height(20.dp))
                     // 검색 결과 기반으로 필드 업데이트
-                    Text("음식 이름: ${foodName}")
-                    Spacer(modifier = Modifier.height(13.dp))
-                    Text("칼로리(kcal): ${calorie}")
-                    Spacer(modifier = Modifier.height(13.dp))
-                    Row(
+                    Text(
+                        "${foodName}",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.CenterHorizontally),
+                        textAlign = TextAlign.Center,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        "칼로리 : ${calorie} kcal",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.CenterHorizontally)
+                            .padding(end = 8.dp),
+                        textAlign = TextAlign.End,
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                            .border(2.dp, Color.Gray, RoundedCornerShape(8.dp))
                     ) {
-                        Column() {
-                            Text("탄수화물(g)")
-                            Text("${carbs}")
-                        }
-                        Spacer(modifier = Modifier.padding(15.dp))
-                        Column() {
-                            Text("단백질(g)")
-                            Text("${protein}")
-                        }
-                        Spacer(modifier = Modifier.padding(15.dp))
-                        Column() {
-                            Text("지방(g)")
-                            Text("${fat}")
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.align(Alignment.Center)
+                        ) {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center,
+                                modifier = Modifier.weight(1f)
+                            ) {
+                                Text("탄수화물", fontSize = 15.sp)
+                                Spacer(modifier = Modifier.padding(5.dp))
+                                Text("${carbs} g", fontSize = 15.sp)
+                            }
+                            // 구분선
+                            Divider(
+                                color = Color.LightGray,
+                                modifier = Modifier
+                                    .height(75.dp)
+                                    .width(1.dp)
+                                    .align(Alignment.CenterVertically)
+                            )
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center,
+                                modifier = Modifier.weight(1f)
+                            ) {
+                                Text("단백질", fontSize = 15.sp)
+                                Spacer(modifier = Modifier.padding(5.dp))
+                                Text("${protein} g", fontSize = 15.sp)
+                            }
+                            // 구분선
+                            Divider(
+                                color = Color.LightGray,
+                                modifier = Modifier
+                                    .height(75.dp)
+                                    .width(1.dp)
+                                    .align(Alignment.CenterVertically)
+                            )
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center,
+                                modifier = Modifier.weight(1f)
+                            ) {
+                                Text("지방(g)", fontSize = 15.sp)
+                                Spacer(modifier = Modifier.padding(5.dp))
+                                Text("${fat} g", fontSize = 15.sp)
+                            }
                         }
                     }
                 }
@@ -147,7 +216,7 @@ fun ShowDropdownMenu(foodLists: List<FoodInfo>, onItemSelected: (FoodInfo) -> Un
     DropdownMenu(
         expanded = true,
         onDismissRequest = { /* 드롭다운 메뉴 닫기 처리 */ },
-        modifier = Modifier.fillMaxWidth().heightIn(max = 200.dp)
+        modifier = Modifier.width(270.dp).heightIn(max = 200.dp)
     ) {
         foodLists.forEach { food ->
             DropdownMenuItem(
